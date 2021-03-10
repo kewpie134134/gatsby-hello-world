ac6c793b285b1232ae414e8c6d78af24e921466a
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-03-11 07:23:16 +0900

    tailwindcssでデザイン作成

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import Meta from "../components/meta";
+import "../utils/styles.css";
 
 const index = () => (
   <Layout>
@@ -12,6 +13,32 @@ const index = () => (
     />
     <div>
       <p>トップページのメインコンテンツ！</p>
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img
+          class="w-full"
+          src="https://source.unsplash.com/random/1600x900/"
+          alt="Sunset in the mountains"
+        ></img>
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div class="px-6 py-4">
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #photography
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #travel
+          </span>
+          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
+            #winter
+          </span>
+        </div>
+      </div>
       <Link to={"/second/"}>Second ページへのリンク</Link>
       <Image filename="sample.jpg" alt="サンプル画像" />
     </div>
