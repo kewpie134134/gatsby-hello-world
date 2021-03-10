2b72eb69a914cf0cbb0fc9702c0accc556db342a
Tomoyuki Baba <bears.pigs.waterimps.48@gmail.com>
2021-03-10 22:45:06 +0900

    CSSなしバージョン

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,6 @@ const index = () => (
     />
     <div>
       <p>トップページのメインコンテンツ！</p>
-      <button class="block uppercase mx-auto shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded">
-        Tailwind test
-      </button>
       <Link to={"/second/"}>Second ページへのリンク</Link>
       <Image filename="sample.jpg" alt="サンプル画像" />
     </div>
